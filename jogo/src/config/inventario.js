7839fc0989d320634f1354823e4631bc86bad52b
NICOLAS RODRIGUES DA LUZ <nicolas_r_luz@estudante.sesisenai.org.br>
2026-08-24 21:37:40 -0300

    a

--- jogo/src/config/inventario.js
+++
@@ -1,9 +0,0 @@
-import leia from 'readline-sync'
-import { PlayerInicial } from './player.js'
-import { potions } from './potions.js'
-import { pokebolas } from './pokebolas.js'
-
-export const inventario = [{
-
-}
-]
